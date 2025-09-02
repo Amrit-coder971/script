80792710a39054f785af356012b26eb43f523c64
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-09-02 16:27:47 +0545

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+const STORAGE_KEY= "todos";
+
+let todos = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+
+const listEl = document.querySelector('#todo-lists');
+
+function handleAdd() {
+    const inputEl = document.getElementById("item-input");
+
+    if(inputEl != ""){
+        todos.push(inputEl.value);
+        inputEl.value="";
+    
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+    
+        listEl.innerHTML = 
+        `${todos.map(todo => 
+        `<li id="todo-${todos.indexOf(todo)}"> ${todo} 
+        </li>`)}`; 
+    }
+
+
+}
+
+
+
+const togglerEl = document.getElementById("mode-toggler");
+
+togglerEl.addEventListener("click", function(event){
+    const isDarkMode = document.body.classList.contains("dark-mode");
+    
+    if(isDarkMode){
+        document.body.setAttribute('class', 'light-mode');
+        togglerEl.innerText = "Change to dark mode";
+    } else {
+        document.body.setAttribute('class', 'dark-mode');
+        togglerEl.innerText = "Change to light mode";
+    }
+});
+
